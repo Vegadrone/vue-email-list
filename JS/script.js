02a0289d6745cce0718eda97e0502523516c6805
Vegadrone <spenaantonino@gmail.com>
2022-07-13 17:45:24 +0200

    Print all email adresses in one li,

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,24 @@ const app = new Vue(
     {
         el:root,
     
-        data:{},
+        data:{
+            
+            email:""
+        },
 
-        methods:{},
+        methods:{
+
+        },
+
+        created(){
+
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        this.email = response.data.response;
+                    });
+            }
+        }
+
     }
 )
